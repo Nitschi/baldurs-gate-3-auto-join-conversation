abef94aa96d84ba379895128f534c9881b5fd919
Alexander Nitsch <alex@nits.ch>
2023-09-01 12:18:08 +0000

    fix version_info.txt

--- build.py
+++ build.py
@@ -37,13 +37,7 @@ with open("version_info.txt", "w") as f:
     f.write(f"""VSVersionInfo(
   ffi=FixedFileInfo(
     filevers=({major}, {minor}, {fix}, 0),
-    prodvers=({major}, {minor}, {fix}, 0),
-    mask=0x3f,
-    flags=0x0,
-    os=0x4,
-    fileType=0x1,
-    subtype=0x0,
-    date=(0, 0)
+    prodvers=({major}, {minor}, {fix}, 0)
     ),
   kids=[
     StringFileInfo(
